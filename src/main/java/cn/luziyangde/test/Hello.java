b2a3ba84150b156074b4fec512abe4623f0b7011
陆云龙 <yunlong@bizfocus.cn>
2021-07-23 17:06:38 +0800

    Add git test

---
+++ src/main/java/cn/luziyangde/test/Hello.java
@@ -0,0 +1,8 @@
+package cn.luziyangde.test;
+
+/**
+ * @author yunlong.lu
+ * @date 2021/07/23
+ **/
+public class Hello {
+}
---
+++ src/main/java/cn/luziyangde/test/TestMain.java
@@ -0,0 +1,11 @@
+package cn.luziyangde.test;
+
+/**
+ * @author yunlong.lu
+ * @date 2021/07/23
+ **/
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello, git");
+    }
+}
